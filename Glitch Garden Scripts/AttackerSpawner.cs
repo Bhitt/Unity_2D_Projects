1d804fcfadbbaf36615300ef84df88797299de15
Bhitt <BrandenHitt@gmail.com>
2020-11-17 17:43:11 -0800

    +glitch garden scripts added for reference
    Signed-off-by: Bhitt <BrandenHitt@gmail.com>

---
+++ Glitch Garden Scripts/AttackerSpawner.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackerSpawner : MonoBehaviour
+{
+    //configuration parameters
+    bool spawn = true;
+    [SerializeField] float minSpawnDelay = 1f;
+    [SerializeField] float maxSpawnDelay = 5f;
+    [SerializeField] Attacker[] attackerPrefabArray;
+
+    // Start is called before the first frame update
+    IEnumerator Start()
+    {
+        while (spawn)
+        {
+            yield return new WaitForSeconds(Random.Range(minSpawnDelay, maxSpawnDelay));
+            SpawnAttacker();
+        }
+    }
+
+    public void StopSpawning()
+    {
+        spawn = false;
+    }
+
+    private void SpawnAttacker()
+    {
+        var attackerIndex = Random.Range(0, attackerPrefabArray.Length - 1);
+        Spawn(attackerPrefabArray[attackerIndex]);
+    }
+
+    private void Spawn(Attacker myAttacker)
+    {
+        /* instance a new attacker */
+        Attacker newAttacker = Instantiate(
+            myAttacker,
+            transform.position,
+            transform.rotation
+            ) as Attacker;
+
+        /* Make new attacker a child to current object */
+        newAttacker.transform.parent = transform;
+    }
+}
